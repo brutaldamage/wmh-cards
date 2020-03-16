802524c5f57eea6a46ea53ca56f3a5ce74e87fe2
Drew Frisk <drew@arbretech.com>
2020-03-16 08:59:45 -0500

    just use regular data.json file, no data.min
    add some cc links to readme for testing purposes

--- WMHCardGenerator.Core/PDFer.cs
+++ WMHCardGenerator.Core/PDFer.cs
@@ -10,163 +10,164 @@ using WMHCardGenerator.Models;
 
 namespace WMHCardGenerator.Core
 {
-	public class PDFer
-	{
-		private HttpClient Http { get; }
+    public class PDFer
+    {
+        private HttpClient Http { get; }
 
-		public PDFer(HttpClient http)
-		{
-			Http = http;
-		}
+        public PDFer(HttpClient http)
+        {
+            Http = http;
+        }
 
-		public async Task<List<Models.GeneratedData>> Generate(string cclink)
-		{
-			List<Models.GeneratedData> PDFLinks = new List<Models.GeneratedData>();
+        public async Task<List<Models.GeneratedData>> Generate(string cclink)
+        {
+            List<Models.GeneratedData> PDFLinks = new List<Models.GeneratedData>();
 
-			if (GetListId(cclink, out string ccid))
-			{
-				var url = $"https://api.conflictchamber.com/list/{ccid}.JSON";
+            if (GetListId(cclink, out string ccid))
+            {
+                var url = $"https://api.conflictchamber.com/list/{ccid}.JSON";
 
-				var ccListInfo = JsonConvert.DeserializeObject<CCInfoResponse>(await Http.GetStringAsync(url));
+                var ccListInfo = JsonConvert.DeserializeObject<CCInfoResponse>(await Http.GetStringAsync(url));
 
-				var data = GetLookupData();
-				string cardUrl = "http://cards.privateerpress.com?card_items_to_pdf=";//$4402,1$4399,1";
+                var data = GetLookupData();
+                string cardUrl = "http://cards.privateerpress.com?card_items_to_pdf=";
 
-				foreach (var list in ccListInfo.Lists)
-				{
-					var caster = list.Models.First(x => x.Type == "Warcaster" || x.Type == "Warlock" || x.Type == "Master");
+                foreach (var list in ccListInfo.Lists)
+                {
+                    var caster = list.Models.First(x => x.Type == "Warcaster" || x.Type == "Warlock" || x.Type == "Master");
 
-					List<string> modelNames = new List<string>();
-					foreach (var model in list.Models)
-					{
-						AddModelName(modelNames, model.Name);
+                    List<string> modelNames = new List<string>();
+                    foreach (var model in list.Models)
+                    {
+                        AddModelName(modelNames, model.Name);
 
-						if (model.Attached != null)
-						{
-							foreach (var attached in model.Attached)
-							{
-								AddModelName(modelNames, attached.Name);
-							}
-						}
-					}
+                        if (model.Attached != null)
+                        {
+                            foreach (var attached in model.Attached)
+                            {
+                                AddModelName(modelNames, attached.Name);
+                            }
+                        }
+                    }
 
-					var grouped = modelNames.GroupBy(x => x);
-					string queryString = null;
+                    var grouped = modelNames.GroupBy(x => x);
+                    string queryString = null;
 
-					var distinct = grouped.Select(x => x.Key);
+                    var distinct = grouped.Select(x => x.Key);
 
-					foreach (var group in grouped)
-					{
-						var value = group.Select(x => x).Where(x => !string.IsNullOrEmpty(x));
+                    foreach (var group in grouped)
+                    {
+                        var value = group.Select(x => x).Where(x => !string.IsNullOrEmpty(x));
 
-						var card = data.FirstOrDefault(x => x.Name.Trim().ToLower() == group.Key.Trim().ToLower() && !string.IsNullOrEmpty(x.CardId));
+                        var card = data.FirstOrDefault(x => x.Name.Trim().ToLower() == group.Key.Trim().ToLower() && !string.IsNullOrEmpty(x.CardId));
 
-						if (card != null)
-						{
-							queryString += $"${card.CardId},{group.Count()}";
-						}
-					}
+                        if (card != null)
+                        {
+                            queryString += $"${card.CardId},{group.Count()}";
+                        }
+                    }
 
-					PDFLinks.Add(new GeneratedData
-					{
-						CasterName = caster.Name,
-						PDFUrl = $"{cardUrl}{queryString}",
-						ListOutput = CreateListDisplayText(ccListInfo.Faction, list)
-					});
-				}
-			}
+                    PDFLinks.Add(new GeneratedData
+                    {
+                        CasterName = caster.Name,
+                        PDFUrl = $"{cardUrl}{queryString}",
+                        ListOutput = CreateListDisplayText(ccListInfo.Faction, list)
+                    });
+                }
+            }
 
-			else
-			{
-				throw new Exception("Unable to parse conflict chamber link. Make sure it is a valid link");
-			}
-			return PDFLinks;
-		}
+            else
+            {
+                throw new Exception("Unable to parse conflict chamber link. Make sure it is a valid link");
+            }
+            return PDFLinks;
+        }
 
-		public bool GetListId(string cclist, out string ccId)
-		{
-			try
-			{
-				var uri = new Uri(cclist);
-				var pathAndQuery = uri.PathAndQuery;
-				ccId = pathAndQuery.Substring(2);
+        public bool GetListId(string cclist, out string ccId)
+        {
+            try
+            {
+                var uri = new Uri(cclist);
+                var pathAndQuery = uri.PathAndQuery;
+                ccId = pathAndQuery.Substring(2);
 
-				return true;
-			}
-			catch (Exception)
-			{
-				ccId = null;
-				return false;
-			}
-		}
+                return true;
+            }
+            catch (Exception)
+            {
+                ccId = null;
+                return false;
+            }
+        }
 
-		public string CreateListDisplayText(string faction, CCListInfoResponse cCInfo)
-		{
-			StringBuilder sb = new StringBuilder();
+        public string CreateListDisplayText(string faction, CCListInfoResponse cCInfo)
+        {
+            StringBuilder sb = new StringBuilder();
 
-			sb.AppendLine(faction);
-			sb.AppendLine();
-			sb.AppendLine($"Theme: {cCInfo.Theme}");
-			sb.AppendLine($"Points: {cCInfo.Points}");
+            sb.AppendLine(faction);
+            sb.AppendLine();
+            sb.AppendLine($"Theme: {cCInfo.Theme}");
+            sb.AppendLine($"Points: {cCInfo.Points}");
 
-			var index = 0;
-			foreach (var model in cCInfo.Models)
-			{
-				if (index == 0)
-				{
-					sb.AppendLine($"[{model.Desc}] {model.Name} [{model.Cost}]");
+            var index = 0;
+            foreach (var model in cCInfo.Models)
+            {
+                if (index == 0)
+                {
+                    sb.AppendLine($"[{model.Desc}] {model.Name} [{model.Cost}]");
+                }
+                else
+                {
+                    sb.AppendLine($"{model.Name} [{model.Cost}]");
+                }
 
-					if (model.Attached != null && model.Attached.Length > 0)
-					{
-						foreach (var attached in model.Attached)
-							sb.AppendLine($"- {attached.Name} [{attached.Cost}]");
-					}
-				}
-				else
-				{
-					sb.AppendLine($"{model.Name} [{model.Cost}]");
-				}
-				index++;
-			}
+                if (model.Attached != null && model.Attached.Length > 0)
+                {
+                    foreach (var attached in model.Attached)
+                        sb.AppendLine($"- {attached.Name} [{attached.Cost}]");
+                }
 
-			return sb.ToString();
-		}
+                index++;
+            }
 
-		List<DataModel> GetLookupData()
-		{
-			var assembly = typeof(PDFer).Assembly;
-			var resourceName = "WMHCardGenerator.Core.data.min.json";
+            return sb.ToString();
+        }
 
-			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-			using (StreamReader reader = new StreamReader(stream))
-			{
-				string result = reader.ReadToEnd();
+        List<DataModel> GetLookupData()
+        {
+            var assembly = typeof(PDFer).Assembly;
+            var resourceName = "WMHCardGenerator.Core.data.json";
 
-				return JsonConvert.DeserializeObject<List<DataModel>>(result);
-			}
-		}
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
 
-		void AddModelName(List<string> modelNames, string name)
-		{
-			var n = name;
-			if (name.Contains("(min)"))
-				n = name.Replace("(min)", string.Empty);
-			else if (name.Contains("(max)"))
-				n = name.Replace("(max)", string.Empty);
+                return JsonConvert.DeserializeObject<List<DataModel>>(result);
+            }
+        }
 
-			// handle req point cards
-			else if (name.EndsWith("(2)"))
-				n = name.Replace("(2)", string.Empty);
-			else if(name.EndsWith("(3)"))
-				n = name.Replace("(3)", string.Empty);
-			else if (name.EndsWith("(4)"))
-				n = name.Replace("(4)", string.Empty);
-			else if (name.EndsWith("(5)"))
-				n = name.Replace("(5)", string.Empty);
-			else if (name.EndsWith("(6)"))
-				n = name.Replace("(6)", string.Empty);
+        void AddModelName(List<string> modelNames, string name)
+        {
+            var n = name;
+            if (name.Contains("(min)"))
+                n = name.Replace("(min)", string.Empty);
+            else if (name.Contains("(max)"))
+                n = name.Replace("(max)", string.Empty);
 
-			modelNames.Add(n.Trim());
-		}
-	}
+            // handle req point cards
+            else if (name.EndsWith("(2)"))
+                n = name.Replace("(2)", string.Empty);
+            else if (name.EndsWith("(3)"))
+                n = name.Replace("(3)", string.Empty);
+            else if (name.EndsWith("(4)"))
+                n = name.Replace("(4)", string.Empty);
+            else if (name.EndsWith("(5)"))
+                n = name.Replace("(5)", string.Empty);
+            else if (name.EndsWith("(6)"))
+                n = name.Replace("(6)", string.Empty);
+
+            modelNames.Add(n.Trim());
+        }
+    }
 }
